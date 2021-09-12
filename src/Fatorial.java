40a5ffded3787987b2c2e05ccd7017256a724664
victor002Hugo <victorsouvilela@hotmail.com>
2021-09-12 17:51:51 -0300

    arquivos para versão recursiva adicionados

---
+++ src/Fatorial.java
@@ -0,0 +1,11 @@
+public class Fatorial {
+
+    public int calcularFatorial(int n){
+
+        int resultado=1;
+        for (int i=2;i<=n;i++){
+            resultado*=i;
+        }
+        return resultado;
+    }
+}
---
+++ src/TesteFatorial.java
@@ -0,0 +1,10 @@
+public class TesteFatorial {
+    public static void main(String[] args) {
+
+        Fatorial fat = new Fatorial();
+
+        int s=5;
+
+        System.out.println(fat.calcularFatorial(s));
+    }
+}
