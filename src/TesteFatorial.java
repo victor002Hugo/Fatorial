b6d6aa41864b79fe282cc9f26e9582a24d89e835
victor002Hugo <victorsouvilela@hotmail.com>
2021-09-12 18:42:15 -0300

    conflito resolvido

--- src/TesteFatorial.java
+++ src/TesteFatorial.java
@@ -1,8 +1,8 @@
 public class TesteFatorial {
     public static void main(String[] args) {
 
-
-                Fatorial fat = new Fatorial();
-                fat.calcularFatorialIterativo(10);
+        Fatorial fat = new Fatorial();
+        int r = fat.calcularFatorialIterativo(4);
+        System.out.println(r);
     }
 }
